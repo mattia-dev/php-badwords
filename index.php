cb462ed2846bbe607edacf30da00236ef9f2af2b
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-08-25 14:24:48 +0200

    refactored

--- index.php
+++ index.php
@@ -20,28 +20,31 @@
             Deleniti vero tempore ipsam, eaque aliquid nam repellat. Autem corporis
             aliquid adipisci eveniet! Vitae quam eveniet similique est, iure impedit eos quisquam!";
 
-        // assegno a una variabile il valore della lunghezza della stringa
-        $paragraphLength = strlen($paragraph);
-
         // assegno a una variabile il valore del parametro get "censor"
         $censoredWord = $_GET['censor'];
 
         // rimpiazzo con tre asterischi la parola che l'utente ha passato per il parametro "censor"
-        $censoredParagraph = str_replace($censoredWord, '***', $paragraph);
+        $paragraph = str_ireplace($censoredWord, '***', $paragraph);
 
-        // // assegno a una variabile il valore della lunghezza del paragrafo censurato
-        $censoredParagraphLength = strlen($censoredParagraph);
+        // assegno a una variabile il valore della lunghezza della stringa
+        $paragraphLength = strlen($paragraph);
+
+        if ($censoredWord != null) {
+            $header = "Il seguente paragrafo conteneva una parola scomoda che è stata censurata ed è lungo ";
+        } else {
+            $header = "Il seguente paragrafo è lungo ";
+        }
 
     ?>
 
-    <!-- stampo la stringa e la sua lunghezza -->
-    <h3>Il seguente paragrafo è lungo <?php echo $paragraphLength ?> caratteri:</h3>
+    <!-- stampo il paragrafo e la sua lunghezza -->
+    <h3>
+        <?php 
+            echo $header;
+            echo $paragraphLength;
+        ?> caratteri:
+    </h3>
     
     <p><?php echo $paragraph ?></p>
-
-    <!-- stampo il paragrafo censurato e la sua lunghezza -->
-    <h3>Il seguente paragrafo conteneva una parola scomoda che è stata censurata ed è lungo <?php echo $censoredParagraphLength ?> caratteri:</h3>
-    
-    <p><?php echo $censoredParagraph ?></p>
 </body>
 </html>
