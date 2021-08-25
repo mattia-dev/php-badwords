9257a9a74092027516d8a3182573c2faf3f6cefa
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-08-25 14:01:09 +0200

    user can now censor a word

--- index.php
+++ index.php
@@ -23,11 +23,25 @@
         // assegno a una variabile il valore della lunghezza della stringa
         $paragraphLength = strlen($paragraph);
 
+        // assegno a una variabile il valore del parametro get "censor"
+        $censoredWord = $_GET['censor'];
+
+        // rimpiazzo con tre asterischi la parola che l'utente ha passato per il parametro "censor"
+        $censoredParagraph = str_replace($censoredWord, '***', $paragraph);
+
+        // // assegno a una variabile il valore della lunghezza del paragrafo censurato
+        $censoredParagraphLength = strlen($censoredParagraph);
+
     ?>
 
     <!-- stampo la stringa e la sua lunghezza -->
     <h3>Il seguente paragrafo è lungo <?php echo $paragraphLength ?> caratteri:</h3>
     
     <p><?php echo $paragraph ?></p>
+
+    <!-- stampo il paragrafo censurato e la sua lunghezza -->
+    <h3>Il seguente paragrafo conteneva una parola scomoda che è stata censurata ed è lungo <?php echo $censoredParagraphLength ?> caratteri:</h3>
+    
+    <p><?php echo $censoredParagraph ?></p>
 </body>
 </html>
